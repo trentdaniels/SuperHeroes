731fff5b947563c69d8ff5572dbe1200bd35e0e2
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-27 14:44:21 -0500

    creating edit method

--- SuperHeroesProject/Controllers/SuperHeroController.cs
+++ SuperHeroesProject/Controllers/SuperHeroController.cs
@@ -11,33 +11,59 @@ namespace SuperHeroesProject.Controllers
     public class SuperHeroController : Controller
     {
         // GET: SuperHero
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult SuperHeroes()
         {
             ApplicationDbContext db = new ApplicationDbContext();
             return View(db.SuperHero);
         }
-        public ActionResult EditHero(int heroId)
+        public ActionResult EditHero(int? id)
         {
             ApplicationDbContext db = new ApplicationDbContext();
-            var selectedHero = db.SuperHero.SingleOrDefault(hero.HeroId == heroId)
-            return View();
+            var selectedHero = db.SuperHero.Find(id);
+            return View(selectedHero);
+        }
+        [HttpPost]
+        public ActionResult EditHero([Bind(Include = "HeroId,Name,AlterEgo, PrimaryAbility, SecondaryAbility,CatchPhrase")] SuperHero hero)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            db.Entry(hero).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("SuperHeroes");
+            
         }
 
-        public ActionResult DeleteHero()
+        public ActionResult DeleteHero(int? id)
         {
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            var selectedHero = db.SuperHero.Find(id);
+            return View(selectedHero);
         }
-        public ActionResult HeroDetails()
+        public ActionResult HeroDetails(int? id)
         {
-            return View();
+            ApplicationDbContext db = new ApplicationDbContext();
+            var selectedHero = db.SuperHero.Find(id);
+            return View(selectedHero);
         }
         public ActionResult CreateHero()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CreateHero(SuperHero hero)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            db.SuperHero.Add(hero);
+            db.SaveChanges();
+            return RedirectToAction("SuperHeroes");
+        }
+        [HttpPost]
+        public ActionResult DeleteHero(int id)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var selectedHero = db.SuperHero.Find(id);
+            db.SuperHero.Remove(selectedHero);
+            db.SaveChanges();
+            return RedirectToAction("SuperHeroes");
         }
     }
 }
