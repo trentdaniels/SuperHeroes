de96d1f59af9a9be26d74238e13416b9dbb9fb9e
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-27 10:01:15 -0500

    initial commit

---
+++ SuperHeroesProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperHeroesProject.MvcApplication" Language="C#" %>
---
+++ SuperHeroesProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperHeroesProject.Startup))]
+namespace SuperHeroesProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
