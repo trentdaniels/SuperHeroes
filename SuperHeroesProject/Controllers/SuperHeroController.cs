55d8c906bcdd3d10b26ab698e3eb7633a22e4ede
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-27 11:20:45 -0500

    created query for list of superheroes

--- SuperHeroesProject/Controllers/SuperHeroController.cs
+++ SuperHeroesProject/Controllers/SuperHeroController.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SuperHeroesProject.Models;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace SuperHeroesProject.Controllers
 {
     public class SuperHeroController : Controller
     {
         // GET: SuperHero
-        public ActionResult SuperHeroes()
+        public ActionResult Index()
         {
             return View();
         }
-        public ActionResult EditHero()
+        public ActionResult SuperHeroes()
         {
+            ApplicationDbContext db = new ApplicationDbContext();
+            return View(db.SuperHero);
+        }
+        public ActionResult EditHero(int heroId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var selectedHero = db.SuperHero.SingleOrDefault(hero.HeroId == heroId)
             return View();
         }
 
--- SuperHeroesProject/Migrations/Configuration.cs
+++ SuperHeroesProject/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace SuperHeroesProject.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using SuperHeroesProject.Models;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<SuperHeroesProject.Models.ApplicationDbContext>
@@ -18,6 +19,53 @@ namespace SuperHeroesProject.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.SuperHero.AddOrUpdate(hero => hero.HeroId,
+                new SuperHero()
+                {
+                    HeroId = 1,
+                    Name = "The Flash",
+                    AlterEgo = "Barry Allen",
+                    PrimaryAbility = "Super Speed",
+                    SecondaryAbility = "Increased Durability",
+                    CatchPhrase = "I'm the fastest man alive"
+                },
+                new SuperHero()
+                {
+                    HeroId = 2,
+                    Name = "Jimmy Neutron",
+                    AlterEgo = "James Isaac Newtron",
+                    PrimaryAbility = "Super Intellect",
+                    SecondaryAbility = "Large Head",
+                    CatchPhrase = "Gotta Blast!"
+                },
+                new SuperHero()
+                {
+                    HeroId = 3,
+                    Name = "The Quickster",
+                    AlterEgo = "Spongebob Squarepants",
+                    PrimaryAbility = "Super Speed",
+                    SecondaryAbility = "Extra Spongey",
+                    CatchPhrase = "Wanna see me run to that mountain and back? Wanna see me do it again?"
+                },
+                new SuperHero()
+                {
+                    HeroId = 4,
+                    Name = "Captain Magma",
+                    AlterEgo = "Squidward Tentacles",
+                    PrimaryAbility = "Cranium Magma Release",
+                    SecondaryAbility = "Terrible Clarinet Playing",
+                    CatchPhrase = "Krakatoa!"
+                },
+                new SuperHero()
+                {
+                    HeroId = 5,
+                    Name = "The Elastic Waistband",
+                    AlterEgo = "Patrick Star",
+                    PrimaryAbility = "Super Stretchiness",
+                    SecondaryAbility = "Stupidity",
+                    CatchPhrase = "Is Mayonnaise an instrument?"
+                });
+
         }
     }
 }
